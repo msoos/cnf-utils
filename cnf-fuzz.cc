06a77c780cc63e285495345c553a4683c68f3c81
Mate Soos <soos.mate@gmail.com>
2012-01-18 22:51:22 +0100

    Also adding gates to fuzzer

--- cnf-fuzz.cc
+++ cnf-fuzz.cc
@@ -18,17 +18,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 #include "MersenneTwister.h"
-
 #include <vector>
+#include "assert.h"
+using std::vector;
 
 class clause {
 public:
     bool is_xor;
-    std::vector<int> literals;
+    vector<int> lits;
 
-    clause()
+    clause(bool _is_xor = false) :
+        is_xor(_is_xor)
     {
     }
 };
@@ -43,74 +44,150 @@ static unsigned int randNorm(double mean, double variance)
     return r;
 }
 
-static std::vector<clause> clauses;
+vector<clause> get_gate(MTRand& rnd, const unsigned nr_variables)
+{
+    vector<clause> ret;
+    size_t gatesize = rnd.randInt(30)+1;
+
+    //Fill literals
+    vector<int> lits(gatesize);
+    for(size_t i = 0; i < gatesize; i++) {
+        bool OK = false;
+        while(!OK) {
+            int thisLit = 1 + rnd.randInt(nr_variables-1);
+            OK = true;
+            for(size_t i2 = 0; i2 < i; i2++) {
+                if (abs(lits[i]) == thisLit) {
+                    OK = false;
+                    break;
+                }
+            }
+            int multiplier = 1 - 2*rnd.randInt(1);
+            lits[i] = thisLit * multiplier;
+        }
+    }
+
+    //The long clause
+    clause cl;
+    cl.lits.push_back(-1*lits[0]);
+    for(size_t i = 1; i < gatesize; i++) {
+        cl.lits.push_back(lits[i]);
+    }
+    ret.push_back(cl);
+
+    //The many short clauses
+    for(size_t i = 1; i < gatesize; i++) {
+        clause cl;
+        cl.lits.push_back(lits[0]);
+        cl.lits.push_back(-1*lits[i]);
+        ret.push_back(cl);
+    }
+
+    return ret;
+}
+
+static vector<clause> clauses;
 bool need_regular_clauses = true;
 
 int main(int, char **)
 {
-    unsigned int nr_variables = randNorm(120, 50);
+    unsigned int nr_variables = randNorm(8000, 300);
+        nr_variables++;
+    assert(nr_variables > 0);
 
-    unsigned int nr_constraint_types = 2 + rnd.randInt(15);
-    for (unsigned int i = 0; i < nr_constraint_types; ++i) {
-        bool is_xor = (rnd.randInt(15) == 1);
-        unsigned nr_literals = 3 + ((rnd.randInt(4) == 1) ? rnd.randInt(40) : rnd.randInt(3));
-        //printf("c literal num: %u\n", nr_literals);
-        unsigned int offsets[nr_literals];
-        bool polarities[nr_literals];
+    unsigned int nr_constraints = 2 + rnd.randInt(4);
+
+    for (unsigned int i = 0; i < nr_constraints; ++i) {
+        //Only every 15th should be an XOR
+        bool is_xor = (rnd.randInt(3) == 1);
+
+        unsigned size;
+        if (rnd.randInt(10) == 1) {
+            size = 1 + randNorm(10, 2);
+        } else {
+            size = 1 + randNorm(3, 2);
+        }
+        unsigned int offsets[size];
+        bool polarities[size];
 
         //Set offsets and polarities
-        for (unsigned int j = 0; j < nr_literals; ++j) {
+        for (unsigned int j = 0; j < size; ++j) {
             offsets[j] = rnd.randInt();
-            polarities[j] = rnd.randInt(2);
+            polarities[j] = rnd.randInt(1);
         }
         unsigned int stride = 1 + rnd.randInt(3);
 
         for (unsigned int j = 0; j < nr_variables; j += stride) {
-            clause c;
-            c.is_xor = is_xor;
+            clause c(is_xor);
 
             //Vary the size of the clause
             if (j % 3 == 1) {
-                nr_literals -= (int)rnd.randInt(2) - 1;
-                if (nr_literals < 1) nr_literals = 3;
+                size -= (int)rnd.randInt(2) - 1;
+                if (size < 1)
+                    size = 3;
             }
-            if (c.is_xor) nr_literals = std::min<unsigned>(nr_literals, 6);
 
-            for (unsigned int k = 0; k < nr_literals; ++k) {
+            //If it's an XOR, don't make it so large
+            if (c.is_xor)
+                size = std::min<unsigned>(size, 6);
+
+            for (unsigned int k = 0; k < size; ++k) {
                 int lit = (polarities[k] ? -1 : 1) * (1 + ((j + offsets[k]) % nr_variables));
-                c.literals.push_back(lit);
-                //printf("literal at this point: %d\n", lit);
+                c.lits.push_back(lit);
             }
-            //printf("clause end\n");
 
-            if (is_xor && need_regular_clauses) {
-                for (unsigned int k = 0; k < (1 << nr_literals); ++k) {
-                    if (__builtin_parity(k))
-                        continue;
-
-                    clause nc;
-                    nc.is_xor = false;
-
-                    for (unsigned int l = 0; l < c.literals.size(); ++l)
-                        nc.literals.push_back(((k & (1 << l)) ? -1 : 1) * c.literals[l]);
-
-                    clauses.push_back(nc);
-                }
-            } else {
-                clauses.push_back(c);
-            }
+            clauses.push_back(c);
         }
+    }
+
+    size_t simpl_rand_constrs = rnd.randInt(5000);
+    for (unsigned int i = 0; i < simpl_rand_constrs; ++i) {
+        size_t size = 1+rnd.randNorm(10,2);
+        clause cl;
+        for(size_t i2 = 0; i2 < size; i2++) {
+            cl.lits.push_back( (1-(2*rnd.randInt(1))) * (1+rnd.randInt(nr_variables-1)) );
+        }
+    }
+
+    size_t other_constraints = randNorm(200, 20);
+    for(size_t i = 0; i < other_constraints; i++) {
+        vector<clause> add_cls = get_gate(rnd, nr_variables);
+        for(size_t i2 = 0; i2 < add_cls.size(); i2++) {
+            clauses.push_back(add_cls[i2]);
+        }
+    }
+
+    for(size_t i = 0; i < 10; i++) {
+        clause cl;
+        cl.lits.push_back( (1-(rnd.randInt(1)*2)) * (1+rnd.randInt(nr_variables-2)) );
+        clauses.push_back(cl);
     }
 
     printf("p cnf %u %lu\n", nr_variables, clauses.size());
     for (unsigned int i = 0; i < clauses.size(); ++i) {
-        clause &c = clauses[i];
+        const clause &c = clauses[i];
 
-        if (c.is_xor)
-            printf("x ");
+        //If XOR, handle differently
+        if (c.is_xor && need_regular_clauses) {
+            for (unsigned int k = 0; k < (1 << c.lits.size()); ++k) {
+                if (__builtin_parity(k))
+                    continue;
 
-        for (unsigned int k = 0; k < c.literals.size(); ++k)
-            printf("%d ", c.literals[k]);
+                for (unsigned int l = 0; l < c.lits.size(); ++l) {
+                    printf("%d ", ((k & (1 << l)) ? -1 : 1) * c.lits[l]);
+                }
+                printf("0\n");
+            }
+        } else {
+            //If XOR, but doesn't need regular clause conversion, print X
+            if (c.is_xor) {
+                printf("x ");
+            }
+
+            //Print the literals
+            for (unsigned int k = 0; k < c.lits.size(); ++k)
+                printf("%d ", c.lits[k]);
+        }
 
         printf("0\n");
     }
