30635d0620c0d5ff60e30902d8ab64dfd531e535
Mate Soos <soos.mate@gmail.com>
2013-04-16 19:46:12 +0200

    Adding system that allows fuzzing part-finding

---
+++ multipart.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import sys, string, os
+
+print "c %s" % sys.argv
+
+ret = ""
+at = 0
+numvarsUntilNow = 0
+for f in sys.argv :
+    at+=1
+    if at == 1 :
+        continue
+
+    thisnumvars = 0
+    ins = open( f, "r" )
+    for line in ins:
+        #ignore header and comments
+        if line[0] == 'p' or line[0] == 'c' :
+            continue
+
+        line = line.rstrip().lstrip()
+        parts = line.split()
+        towrite = ""
+        for part in parts :
+            #end of line
+            if (part == "0") :
+                towrite += "0"
+                #sys.stdout.write("0\n")
+                break
+
+            #update number of variables in this part
+            if int(part) > thisnumvars :
+                thisnumvars = int(part)
+
+            #increment variable number if need be
+            newLit = abs(int(part)) + numvarsUntilNow
+
+            #invert if needed
+            if (int(part) < 0) :
+                newLit = -1*newLit
+
+            #write updated literal
+            towrite += "%s " % newLit #sys.stdout.write("%d " % newLit)
+
+        #end of this line in file
+        print towrite
+
+    #next part has to be updated with incremented varaibles
+    numvarsUntilNow += thisnumvars
+
+
+
+
+#file1 = sys.argv[1]
+#file2 = sys.argv[2]
