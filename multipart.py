d7681ec30b22143311fb67b222740e1e8790d137
Mate Soos <soos.mate@gmail.com>
2013-04-23 23:56:18 +0200

    Create header for multipart CNF file

--- multipart.py
+++ multipart.py
@@ -4,6 +4,29 @@ import sys, string, os
 
 print "c %s" % sys.argv
 
+
+#create header
+headerNumVars = 0
+headerNumCls = 0
+for f in sys.argv :
+    at+=1
+    if at == 1 :
+        continue
+
+    thisnumvars = 0
+    ins = open( f, "r" )
+    line = line.rstrip().lstrip()
+        if line[0] == 'p' or line[0] == 'c' :
+            continue
+
+        parts = line.split()
+        for part in parts :
+            headerNumVars = max(headerNumVars, int(part)
+        headerNumCls+=1
+
+print "p cnf %d %d" % (headerNumVars, headerNumCls)
+
+#print final CNF
 ret = ""
 at = 0
 numvarsUntilNow = 0
