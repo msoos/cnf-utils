1cd30613fc6b769ecdf36b856da11d0e39d89cd0
Mate Soos <soos.mate@gmail.com>
2015-09-09 22:11:46 +0100

    PEP8-ing the xortester

--- xortester.py
+++ xortester.py
@@ -1,78 +1,76 @@
 #!/usr/bin/env python
 
-import random, sys
+import random
+import sys
 
-numvars = random.randint(500,10000)
-numunits = random.randint(0,15)
-numlongs = random.randint(100, numvars*2)
+numvars = random.randint(500, 10000)
+numunits = random.randint(0, 15)
+numlongs = random.randint(100, numvars * 2)
 numcls = numunits + numlongs
 
 xorclsizes = []
-for i in range(random.randint(100, numvars)) :
-    thissize = random.randint(3,7)
+for i in range(random.randint(100, numvars)):
+    thissize = random.randint(3, 7)
     xorclsizes.append(thissize)
-    numcls += 1 << (thissize-1)
+    numcls += 1 << (thissize - 1)
 
 print "p cnf %d %d" % (numvars, numcls)
 
-#longcls
-for i in range(numlongs) :
-    for i2 in range(random.randint(2,5)) :
-        lit = random.randint(1,numvars)
-        if random.randint(0,1) == 1 :
-            lit = -1*lit
+# longcls
+for i in range(numlongs):
+    for i2 in range(random.randint(2, 5)):
+        lit = random.randint(1, numvars)
+        if random.randint(0, 1) == 1:
+            lit = -1 * lit
         sys.stdout.write("%d " % lit)
 
     print "0"
 
-#units
-for i in range(numunits) :
-    lit = random.randint(1,numvars)
-    if random.randint(0,1) == 1 :
-        lit = -1*lit
+# units
+for i in range(numunits):
+    lit = random.randint(1, numvars)
+    if random.randint(0, 1) == 1:
+        lit = -1 * lit
     print "%d 0" % lit
 
-#xors
-for thisxorsize in xorclsizes :
+# xors
+for thisxorsize in xorclsizes:
     varlist = []
 
-    #create varlist
-    for a in range(thisxorsize) :
-        var = random.randint(0,numvars)
-        while var in varlist :
-            var = random.randint(1,numvars)
+    # create varlist
+    for a in range(thisxorsize):
+        var = random.randint(0, numvars)
+        while var in varlist:
+            var = random.randint(1, numvars)
 
-        #flip randomly
-        if random.randint(0,1) == 1 :
-            var = -1*var
+        # flip randomly
+        if random.randint(0, 1) == 1:
+            var = -1 * var
 
         varlist.append(var)
 
-    #polarity of the XOR
-    polarity = random.randint(0,1)
+    # polarity of the XOR
+    polarity = random.randint(0, 1)
 
-    for i2 in range(1<<len(varlist)) :
-        #number of inversions is right, use it
-        if bin(i2).count("1") % 2 == polarity :
+    for i2 in range(1 << len(varlist)):
+        # number of inversions is right, use it
+        if bin(i2).count("1") % 2 == polarity:
             at = 0
-            for var in varlist :
+            for var in varlist:
                 lit = var
 
-                #calculate inversion
-                invert = (i2 >> at)&1 == 1
-                #if polarity :
+                # calculate inversion
+                invert = (i2 >> at) & 1 == 1
+                # if polarity :
                 #    invert = not invert
 
-                #create lit
+                # create lit
                 if invert:
-                    lit = -1* var
+                    lit = -1 * var
 
-                #print lit
+                # print lit
                 sys.stdout.write("%d " % lit)
-                at+=1
+                at += 1
 
-            #end of clause
+            # end of clause
             print "0"
-
-
-
