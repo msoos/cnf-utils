a5fbc4aab75dda9072d43ca9e43bde52ae73726b
Mate Soos <soos.mate@gmail.com>
2016-02-16 21:58:10 +0000

    Updating matrix python script

--- gen_matrix_based_cnf.py
+++ gen_matrix_based_cnf.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+print("NOTE: this should import sage in some way")
+
 def gen_cnf(n):
     A = random_matrix(GF(2),n,n+1)
     B = BooleanPolynomialRing(n,'x')
@@ -8,4 +10,10 @@ def gen_cnf(n):
     l = (A*v).list()
     a2 = ANFSatSolver(B)
     s = a2.cnf(l)
-    open("matrix%02d.cnf"%n,"w").write(s)
+
+    fname = "matrix%02d.cnf" % n
+    print("written matrix CNF to file %s" % fname)
+    open(fname).write(s)
+
+
+gen_cnf(1)
