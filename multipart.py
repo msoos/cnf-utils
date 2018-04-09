48641f5b74ec2b50c0cebbb647d761955e2ad133
Mate Soos <soos.mate@gmail.com>
2018-04-09 14:11:07 +0800

    PEP8-ing

--- multipart.py
+++ multipart.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 from __future__ import with_statement
-import sys, string, os
+import sys
+import string
 
 print("c %s" % sys.argv)
 
 
-#create header
+# create header
 headerNumVars = 0
 headerNumCls = 0
 at = 0
-for fname in sys.argv :
-    at+=1
-    if at == 1 :
+for fname in sys.argv:
+    at += 1
+    if at == 1:
         continue
 
     thisnumvars = 0
-    with open( fname, "r" ) as ins :
-        for line in ins :
-            if line[0] == 'p' or line[0] == 'c' :
+    with open(fname, "r") as ins:
+        for line in ins:
+            if line[0] == 'p' or line[0] == 'c':
                 continue
 
-            for part in line.split() :
+            for part in line.split():
                 if part.strip() == 'x':
                     continue
                 thisnumvars = max(thisnumvars, abs(int(part)))
@@ -30,20 +31,20 @@ for fname in sys.argv :
 
 print("p cnf %d %d" % (headerNumVars, headerNumCls))
 
-#print final CNF
+# print final CNF
 ret = ""
 at = 0
 numvarsUntilNow = 0
-for f in sys.argv :
-    at+=1
-    if at == 1 :
+for f in sys.argv:
+    at += 1
+    if at == 1:
         continue
 
     thisnumvars = 0
-    with open( f, "r" ) as ins :
+    with open(f, "r") as ins:
         for line in ins:
-            #ignore header and comments
-            if line[0] == 'p' or line[0] == 'c' :
+            # ignore header and comments
+            if line[0] == 'p' or line[0] == 'c':
                 continue
 
             line = line.rstrip().lstrip()
@@ -54,30 +55,26 @@ for f in sys.argv :
                     towrite += "x "
                     continue
 
-                #end of line
-                if (part == "0") :
+                # end of line
+                if (part == "0"):
                     towrite += "0"
-                    #sys.stdout.write("0\n")
                     break
 
-                #update number of variables in this part
+                # update number of variables in this part
                 thisnumvars = max(thisnumvars, abs(int(part)))
 
-                #increment variable number if need be
+                # increment variable number if need be
                 newLit = abs(int(part)) + numvarsUntilNow
 
-                #invert if needed
-                if (int(part) < 0) :
+                # invert if needed
+                if (int(part) < 0):
                     newLit = -1*newLit
 
-                #write updated literal
-                towrite += "%d " % newLit #sys.stdout.write("%d " % newLit)
+                # write updated literal
+                towrite += "%d " % newLit
 
-            #end of this line in file
+            # end of this line in file
             print(towrite)
 
-    #next part has to be updated with incremented varaibles
+    # next part has to be updated with incremented varaibles
     numvarsUntilNow += thisnumvars
-
-#file1 = sys.argv[1]
-#file2 = sys.argv[2]
