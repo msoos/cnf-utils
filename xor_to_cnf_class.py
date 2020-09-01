aa0ac24b15d7d7d69057d6d87256eaec65cd49d3
Mate Soos <soos.mate@gmail.com>
2020-09-01 23:43:45 +0200

    Fixing licensing
    Closes #3

--- xor_to_cnf_class.py
+++ xor_to_cnf_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2014  Mate Soos
+# Copyright (C) 2018  Mate Soos
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -15,7 +15,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 
 from __future__ import print_function
 import re
